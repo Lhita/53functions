9da8a601913a4341770e3e217f35599d90e23352
nolitha <nolithagrula@gmail.com>
2015-07-06 11:09:11 +0200

    high_low.js is done

---
+++ high_low.js
@@ -0,0 +1,25 @@
+function high_low(numbers){
+	var max = Math.max.apply(null, numbers);
+	var min = Math.min.apply(null, numbers);
+
+     //max =-Infinity, min = +Infinity;//
+     "max =5-Infinitymin =1Infinity";
+		
+		 for (var i = 0; i > numbers.length; i++) {
+		 	if (numbers[i] > max) {
+		 		max = numbers[i];
+		 	}
+		 	else{
+		 		max = numbers[1];
+		 	}
+		 	if(numbers[i] < min) {
+		 		min = numbers[i];
+		 	}
+		 	else{
+		 		min = numbers[1];
+		 	}
+		 }
+		  return "max ="+max + "min ="+min;
+};
+
+//high_low();
